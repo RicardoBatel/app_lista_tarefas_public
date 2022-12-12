8e364941cb1e82e11d061832e725cea61a9a1ce8
RIcardo Batel <ricardo.batel@hotmail.com>
2022-12-12 17:09:54 -0300

    alualizando registros

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -20,33 +20,50 @@
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
 		<script>
-			function editar(){
+			function editar(id, txt_tarefa){
 
 				//criar um form programático para edição
 				let form = document.createElement('form')
 				form.action = '#'
 				form.method = 'post'
+				form.className = 'row'
 
 				//criar um input para a entarda do texto
 				let inputTarefa = document.createElement('input')
 				inputTarefa.type = 'text'
 				inputTarefa.name = 'tarefa'
-				inputTarefa.className = 'form=control'
+				inputTarefa.className = 'col-9 form=control'
+				inputTarefa.value = txt_tarefa
+
+				//criar um input hidden para guardar o id da tarefa
+				let inputId = document.createElement('input')
+				inputId.type = 'hidden'
+				inputId.name = 'id'
+				inputId.value = id
 
 				//criar um button para envio do form
 				let button = document.createElement('button')
 				button.type = 'submit'
 				button.className = 'btn btn-info'
-				buttoninnerHTML = 'Atualizar'
+				button.innerHTML = 'Atualizar'
 
 				//incluir inputTarefa no form
 				form.appendChild(inputTarefa)
 
+				//incluir inputId no form
+				form.appendChild(inputId)
+
 				//incluir button no form
 				form.appendChild(button)
 
-				//test
-				console.log(form)
+				//selecionar a div tarefa
+				let tarefa = document.getElementById('tarefa_'+id)
+
+				//limpar o texto da tarefa para inclusão do form
+				tarefa.innerHTML = ''
+
+				//icluir form na página
+				tarefa.insertBefore(form, tarefa[0])
 
 			}
 		</script>
@@ -82,10 +99,12 @@
 
 								<? foreach ($tarefas as $indice => $tarefa) { ?>
 									<div class="row mb-3 d-flex align-items-center tarefa">
-										<div class="col-sm-9"><?= $tarefa->tarefa ?> (<?= $tarefa->status ?>)</div>
+										<div class="col-sm-9" id="tarefa_<?= $tarefa->id ?>">
+											<?= $tarefa->tarefa ?> (<?= $tarefa->status ?>)
+										</div>
 										<div class="col-sm-3 mt-2 d-flex justify-content-between">
 											<i class="fas fa-trash-alt fa-lg text-danger"></i>
-											<i class="fas fa-edit fa-lg text-info" onclick="editar()"></i>
+											<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
 											<i class="fas fa-check-square fa-lg text-success"></i>
 										</div>
 									</div>
