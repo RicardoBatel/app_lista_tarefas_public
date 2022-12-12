aa1c48ce7309f535e700f30f748f85a7f7f71629
RIcardo Batel <ricardo.batel@hotmail.com>
2022-12-12 19:51:25 -0300

    Atualizando registros

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -24,7 +24,7 @@
 
 				//criar um form programático para edição
 				let form = document.createElement('form')
-				form.action = '#'
+				form.action = 'tarefa_controller.php?acao=atualizar'
 				form.method = 'post'
 				form.className = 'row'
 
