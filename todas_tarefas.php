977abf69635e97ee385e67eb908f132272e7ec4f
RIcardo Batel <ricardo.batel@hotmail.com>
2022-12-12 20:54:01 -0300

    Marcando tarefas realizadas

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -2,11 +2,13 @@
 
 	$acao = 'recuperar';
 	require 'tarefa_controller.php';
-/*
+
+	/*
 	echo '<pre>';
 	print_r($tarefas);
 	echo '</pre>';
-*/
+	*/
+
 ?>
 
 <html>
@@ -20,19 +22,19 @@
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
 		<script>
-			function editar(id, txt_tarefa){
+			function editar(id, txt_tarefa) {
 
-				//criar um form programático para edição
+				//criar um form de edição
 				let form = document.createElement('form')
 				form.action = 'tarefa_controller.php?acao=atualizar'
 				form.method = 'post'
 				form.className = 'row'
 
-				//criar um input para a entarda do texto
+				//criar um input para entrada do texto
 				let inputTarefa = document.createElement('input')
 				inputTarefa.type = 'text'
 				inputTarefa.name = 'tarefa'
-				inputTarefa.className = 'col-9 form=control'
+				inputTarefa.className = 'col-9 form-control'
 				inputTarefa.value = txt_tarefa
 
 				//criar um input hidden para guardar o id da tarefa
@@ -44,7 +46,7 @@
 				//criar um button para envio do form
 				let button = document.createElement('button')
 				button.type = 'submit'
-				button.className = 'btn btn-info'
+				button.className = 'col-3 btn btn-info'
 				button.innerHTML = 'Atualizar'
 
 				//incluir inputTarefa no form
@@ -56,23 +58,28 @@
 				//incluir button no form
 				form.appendChild(button)
 
+				//teste
+				//console.log(form)
+
 				//selecionar a div tarefa
 				let tarefa = document.getElementById('tarefa_'+id)
 
 				//limpar o texto da tarefa para inclusão do form
 				tarefa.innerHTML = ''
 
-				//icluir form na página
+				//incluir form na página
 				tarefa.insertBefore(form, tarefa[0])
 
 			}
 
-			function remover(id){
+			function remover(id) {
 				location.href = 'todas_tarefas.php?acao=remover&id='+id;
 			}
 
+			function marcarRealizada(id) {
+				location.href = 'todas_tarefas.php?acao=marcarRealizada&id='+id;
+			}
 		</script>
-
 	</head>
 
 	<body>
@@ -102,19 +109,22 @@
 								<h4>Todas tarefas</h4>
 								<hr />
 
-								<? foreach ($tarefas as $indice => $tarefa) { ?>
+								<?php foreach($tarefas as $indice => $tarefa) { ?>
 									<div class="row mb-3 d-flex align-items-center tarefa">
 										<div class="col-sm-9" id="tarefa_<?= $tarefa->id ?>">
 											<?= $tarefa->tarefa ?> (<?= $tarefa->status ?>)
 										</div>
 										<div class="col-sm-3 mt-2 d-flex justify-content-between">
 											<i class="fas fa-trash-alt fa-lg text-danger" onclick="remover(<?= $tarefa->id ?>)"></i>
-											<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
-											<i class="fas fa-check-square fa-lg text-success"></i>
+											
+											<?php if($tarefa->status == 'pendente') { ?>
+												<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
+												<i class="fas fa-check-square fa-lg text-success" onclick="marcarRealizada(<?= $tarefa->id ?>)"></i>
+											<?php } ?>
 										</div>
 									</div>
 
-								<? } ?>
+								<?php } ?>
 								
 							</div>
 						</div>
